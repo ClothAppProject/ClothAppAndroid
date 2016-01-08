8652db7b479ab24a8b1841b16cccefaf4b2b7bbb
c-simone <simoneconia@gmail.com>
2016-01-08 02:17:15 +0100

    Fix: Clicking the Sign up button wouldn't start sign up process.

--- app/src/main/java/com/clothapp/SignupActivity.java
+++ app/src/main/java/com/clothapp/SignupActivity.java
@@ -212,6 +212,9 @@ public class SignupActivity extends AppCompatActivity {
                                     });
                                 }
                             });
+
+                            // Start the signup thread
+                            signup.start();
                         }
                         break;
                 }
