133210ff09320d523ba440632e6dfcdc9435e0fc
c-simone <simoneconia@gmail.com>
2016-01-10 00:24:14 +0100

    Remove unused import.

--- app/src/main/java/com/clothapp/MainActivity.java
+++ app/src/main/java/com/clothapp/MainActivity.java
@@ -2,7 +2,6 @@ package com.clothapp;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
