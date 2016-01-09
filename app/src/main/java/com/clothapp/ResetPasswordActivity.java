eec80c4f6aecd69d424512dee9d3d4c93daee544
c-simone <simoneconia@gmail.com>
2016-01-10 00:44:54 +0100

    Reformat code and optimize imports.

--- app/src/main/java/com/clothapp/ResetPasswordActivity.java
+++ app/src/main/java/com/clothapp/ResetPasswordActivity.java
@@ -2,10 +2,8 @@ package com.clothapp;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Environment;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,37 +12,40 @@ import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.RequestPasswordResetCallback;
 
-import java.io.File;
-
 import static com.clothapp.resources.ExceptionCheck.check;
-
 import static com.clothapp.resources.RegisterUtil.isValidEmailAddress;
+
 /**
  * Created by giacomoceribelli on 09/01/16.
  */
-public class ResetPasswordActivity extends AppCompatActivity{
+public class ResetPasswordActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_reset_password);
+
         final EditText editEmail = (EditText) findViewById(R.id.mainEmail);
+
         Button reset = (Button) findViewById(R.id.mainReset);
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                if (!isValidEmailAddress(editEmail.getText().toString()))    {
+
+                if (!isValidEmailAddress(editEmail.getText().toString())) {
                     Snackbar.make(v, "Email non valida", Snackbar.LENGTH_LONG).setAction("Action", null).show();
-                }else{
+
+                } else {
                     ParseUser.requestPasswordResetInBackground(editEmail.getText().toString(), new RequestPasswordResetCallback() {
                         public void done(ParseException e) {
+
                             if (e == null) {
-                                //email inviata correttamente
+                                // email inviata correttamente
                                 Snackbar.make(v, "Email di reset inviata correttamente", Snackbar.LENGTH_LONG).setAction("Action", null).show();
                             } else {
-                                //errore nel reperire l'email
-                                check(e.getCode(),v,e.getMessage());
+                                // Errore nel reperire l'email
+                                check(e.getCode(), v, e.getMessage());
                             }
                         }
                     });
@@ -52,11 +53,14 @@ public class ResetPasswordActivity extends AppCompatActivity{
             }
         });
     }
+
     @Override
     public void onBackPressed() {
+
         // Reinderizzo l'utente alla main activity
         Intent i = new Intent(getApplicationContext(), MainActivity.class);
         startActivity(i);
+
         finish();
     }
 }
