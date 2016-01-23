4b19d0d68d397024428eed62700ce0f84ac04d9c
jacopo <jack1395@hotmail.it>
2016-01-23 19:41:54 +0100

    adattata vista della gallery in base al numero di pixel

--- app/src/main/java/com/clothapp/resources/ImageAdapter.java
+++ app/src/main/java/com/clothapp/resources/ImageAdapter.java
@@ -1,8 +1,12 @@
 package com.clothapp.resources;
 
 import android.content.Context;
+import android.graphics.Point;
+import android.util.DisplayMetrics;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
@@ -47,9 +51,16 @@ public class ImageAdapter extends BaseAdapter {
         if (convertView == null) {
             // if it's not recycled, initialize some attributes
             imageView = new ImageView(mContext);
-            imageView.setLayoutParams(new GridView.LayoutParams(600, 600));
+            //TODO: per ora funziona solo con lo schermo verticale
+            //adatto la grandezza delle immagini alla grandezza del display
+            DisplayMetrics metrics =mContext.getResources().getDisplayMetrics();
+
+            int w=metrics.widthPixels;
+            int s=(w/2)-1;
+            System.out.println(s);
+            imageView.setLayoutParams(new GridView.LayoutParams(s, s));
             imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-            imageView.setPadding(8, 8, 8, 8);
+            imageView.setPadding(0,0,0,0);
         } else {
             imageView = (ImageView) convertView;
         }
@@ -61,4 +72,5 @@ public class ImageAdapter extends BaseAdapter {
 
         return imageView;
     }
+
 }
