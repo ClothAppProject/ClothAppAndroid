e611b917b17b7ec1550436c7e02b7e19f43bc8f7
jacopo1395 <jack1395@hotmail.it>
2016-02-07 22:56:13 +0100

    Prima implementazione di TopRated

--- app/src/main/java/com/clothapp/home_gallery/HomeTopRatedFragment.java
+++ app/src/main/java/com/clothapp/home_gallery/HomeTopRatedFragment.java
@@ -1,22 +1,106 @@
 package com.clothapp.home_gallery;
 
+import android.content.Intent;
+
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.clothapp.R;
+import com.clothapp.resources.ApplicationSupport;
+import com.clothapp.resources.Image;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.clothapp.resources.ExceptionCheck.check;
 
 /**
  * Created by giacomoceribelli on 02/02/16.
  */
 public class HomeTopRatedFragment extends Fragment {
+    private List<CardView>cardViews;
+    private View rootView;
+    private View view;
+    private ListView l;
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View rootView = inflater.inflate(R.layout.fragment_home_top_rated, container, false);
+        rootView = inflater.inflate(R.layout.fragment_home_top_rated, container, false);
+        l=(ListView)rootView.findViewById(R.id.toplist);
+        findTopRated();
 
+        //setCardViews(findTopRated(),container);
         return rootView;
     }
+
+    public void setCardViews(List<Image>images,ViewGroup container){
+        int i;
+        for(i=0;i<images.size();i++){
+           // Card c=new Card();
+            ImageView imageView= (ImageView) getActivity().findViewById(R.id.topfoto);
+            l.addView(imageView);
+            Glide.with(this)
+                    .load(images.get(i).getFile())
+                    .into(imageView);
+        }
+    }
+
+    public List<Image> findTopRated(){
+        //qui scarico le foto
+        final View vi = new View(getActivity().getApplicationContext());
+        final ArrayList<Image> photo = new ArrayList<>();
+        final ParseQuery<ParseObject> query = new ParseQuery<ParseObject>("Photo");
+        query.setLimit(4);
+        query.orderByDescending("nLike");
+        query.findInBackground(new FindCallback<ParseObject>() {
+            public void done(List<ParseObject> fotos, ParseException e) {
+                if (e == null) {
+                    Log.d("toprated", "Retrieved " + fotos.size() + " photos");
+
+                    int i;
+                    for (i = 0; i < fotos.size(); i++) {
+                        ParseObject obj = fotos.get(i);
+                        ParseFile file = obj.getParseFile("thumbnail");
+                        try {
+                            //inserisco le foto in una lista
+                            photo.add(new com.clothapp.resources.Image(file.getFile(), obj.getObjectId(),obj.getList("like")));
+                            Log.d("toprated", "finito"+i);
+                        } catch (ParseException e1) {
+                            check(e.getCode(), vi, e.getMessage());
+                        }
+                    }
+                    MyListAdapter adapter=new MyListAdapter(getActivity().getApplicationContext(), photo);
+                    l.setAdapter(adapter);
+                } else {
+                    //errore nel reperire gli oggetti Photo dal database
+                    check(e.getCode(), vi, e.getMessage());
+                }
+            }
+        });
+        return photo;
+    }
+
+
 }
---
+++ app/src/main/java/com/clothapp/home_gallery/MyListAdapter.java
@@ -0,0 +1,95 @@
+package com.clothapp.home_gallery;
+
+
+
+import android.graphics.BitmapFactory;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.clothapp.R;
+
+/**
+ * Created by jack1 on 07/02/2016.
+ */
+import android.content.Context;
+import android.content.Intent;
+import android.util.DisplayMetrics;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.clothapp.ImageFragment;
+import com.clothapp.R;
+import com.clothapp.resources.Image;
+import com.clothapp.resources.SquaredImageView;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static android.widget.ImageView.ScaleType.CENTER_CROP;
+
+public class MyListAdapter extends BaseAdapter {
+    private final Context context;
+    private List<Image> files=new ArrayList<>();
+
+    public MyListAdapter(Context context, List<Image> photos) {
+        this.context = context;
+        files = photos;
+    }
+    public void addToGridView(Image foto)   {files.add(foto);}
+
+    @Override
+    public View getView(final int position, final View convertView, ViewGroup parent) {
+        View row = convertView;
+        if (row==null) {
+            //se la convertView di quest'immagine è nulla la inizializzo
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            row = inflater.inflate(R.layout.fragment_cardlist, parent, false);
+        }
+        final Image image = getItem(position);
+        //prendo i vari oggetti del gridview_layout
+        ImageView view = (ImageView) row.findViewById(R.id.topfoto);
+        TextView textView=(TextView) row.findViewById(R.id.info_text);
+        textView.setText((CharSequence)image.getObjectId());
+
+
+        // Get the image URL for the current position.
+        File file = image.getFile();
+        // Trigger the download of the URL asynchronously into the image view.
+
+        Glide.with(context)
+                .load(file)
+                .centerCrop()
+                .placeholder(R.mipmap.gallery_icon)
+                .into(view);
+
+        return row;
+    }
+
+    @Override public int getCount() {
+        return files.size();
+    }
+
+    @Override public Image getItem(int position) {
+        return (files.get(position));
+    }
+
+    @Override public long getItemId(int position) {
+        return position;
+    }
+}
