d3b73c1933aa101e92dd10bd4a45418ff0575aeb
c-simone <simoneconia@gmail.com>
2016-02-14 02:02:32 +0100

    Add side menu to UserProfileActivity. Other minor changes include snapping for the collapsing toolbar.

--- app/src/main/java/com/clothapp/profile/UserProfileActivity.java
+++ app/src/main/java/com/clothapp/profile/UserProfileActivity.java
@@ -2,22 +2,32 @@ package com.clothapp.profile;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.clothapp.R;
@@ -36,11 +46,11 @@ public class UserProfileActivity extends AppCompatActivity {
 
     private ViewPager mViewPager;
 
+    private DrawerLayout mDrawerLayout;
+
     static Context context;
 
     static String username;
-
-    // static ListView listProfileInfo;
 
     static RecyclerView viewProfileInfo;
 
@@ -58,6 +68,28 @@ public class UserProfileActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("");
         setSupportActionBar(toolbar);
+
+        final ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu_24dp_white);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.com_facebook_profile_picture_blank_square);
+
+        RoundedBitmapDrawable rounded = RoundedBitmapDrawableFactory.create(getResources(), bitmap);
+
+        rounded.setCornerRadius(bitmap.getWidth());
+
+        // ImageView drawerProfile = (ImageView) findViewById(R.id.menu_profile_side_drawer_image);
+        View headerLayout = navigationView.getHeaderView(0);
+        ImageView drawerProfile = (ImageView) headerLayout.findViewById(R.id.menu_profile_side_drawer_image);
+        drawerProfile.setImageDrawable(rounded);
 
         // Create the adapter that will return a fragment for each of the
         // primary sections of the activity.
@@ -81,19 +113,33 @@ public class UserProfileActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+
+            case android.R.id.home:
+                // Log.d("UserProfileActivity", "android.R.id.home");
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+            case R.id.action_settings:
+                // Log.d("UserProfileActivity", "R.id.action_settings");
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
     }
 
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+            new NavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(MenuItem menuItem) {
+                    menuItem.setChecked(true);
+                    mDrawerLayout.closeDrawers();
+                    return true;
+                }
+            });
+    }
 
     // This is a placeholder fragment for unimplemented sections.
     public static class PlaceholderFragment extends Fragment {
