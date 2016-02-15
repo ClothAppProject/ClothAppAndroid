5a1c539820495e1987672620efc0a4b704cdc00d
c-simone <simoneconia@gmail.com>
2016-02-15 15:04:51 +0100

    Add inline documentation to ProfileInfoFragment.java.

--- app/src/main/java/com/clothapp/profile/ProfileInfoFragment.java
+++ app/src/main/java/com/clothapp/profile/ProfileInfoFragment.java
@@ -32,21 +32,30 @@ public class ProfileInfoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        // Inflate the fragment which will contain the RecyclerView
         View rootView = inflater.inflate(R.layout.fragment_profile_info, container, false);
 
+        // Set the recycler view declared in UserProfileActivity to the newly created RecyclerView
         UserProfileActivity.viewProfileInfo = (RecyclerView) rootView.findViewById(R.id.profile_info_recycler_view);
 
+        // Set the layout manager for the recycler view.
+        // LinearLayoutManager makes the recycler view look like a ListView.
         LinearLayoutManager llm = new LinearLayoutManager(UserProfileActivity.context);
         UserProfileActivity.viewProfileInfo.setLayoutManager(llm);
 
-        ArrayList<ProfileInfoListItem> items = new ArrayList<>();
-
+        // itemDummy will be replace by a header
         ProfileInfoListItem itemDummy = new ProfileInfoListItem("DUMMY", "Loading...");
+
+        // Create a new item for each list (recycler view) entry
         ProfileInfoListItem itemName = new ProfileInfoListItem("NAME", "Loading...");
         ProfileInfoListItem itemAge = new ProfileInfoListItem("AGE", "Loading...");
         ProfileInfoListItem itemCity = new ProfileInfoListItem("CITY", "Loading...");
         ProfileInfoListItem itemEmail = new ProfileInfoListItem("EMAIL", "Loading...");
         ProfileInfoListItem itemDescription = new ProfileInfoListItem("DESCRIPTION", "Loading...");
+
+        // Create an array and add the previously created items to it.
+        ArrayList<ProfileInfoListItem> items = new ArrayList<>();
 
         items.add(itemDummy);
         items.add(itemName);
@@ -55,12 +64,14 @@ public class ProfileInfoFragment extends Fragment {
         items.add(itemEmail);
         items.add(itemDescription);
 
+        // Create a new adapter for the recycler view
         ProfileInfoAdapter adapter = new ProfileInfoAdapter(items);
         UserProfileActivity.viewProfileInfo.setAdapter(adapter);
 
         // Get user info from Parse
         ProfileUtils.getParseInfo(UserProfileActivity.context, UserProfileActivity.username);
 
+        // Return the fragment
         return rootView;
     }
 
