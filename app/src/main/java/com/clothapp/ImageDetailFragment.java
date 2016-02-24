c9bc76c4853466da620f5aca63adbc9618459a27
ceribbo <giacomoceribelli@gmail.com>
2016-02-25 00:43:46 +0100

    implementato double tap con like e animazione sulle foto

--- app/src/main/java/com/clothapp/ImageDetailFragment.java
+++ app/src/main/java/com/clothapp/ImageDetailFragment.java
@@ -9,12 +9,16 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
+import android.view.GestureDetector;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -48,7 +52,8 @@ import static com.clothapp.resources.ExceptionCheck.check;
 
 public class ImageDetailFragment extends Fragment {
 
-    private ImageView v;
+    private ImageView imageView;
+    private ImageView heartAnim;
     private TextView t;
     private String Id;
     private Image immagine;
@@ -127,7 +132,8 @@ public class ImageDetailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_screen_slide_page, container, false);
         t=(TextView)rootView.findViewById(R.id.user);
-        v=(ImageView) rootView.findViewById(R.id.photo);
+        imageView=(ImageView) rootView.findViewById(R.id.photo);
+        heartAnim=(ImageView) rootView.findViewById(R.id.heart_anim);
         listView=(ListView)rootView.findViewById(R.id.listInfo);
         hashtag=(TextView)rootView.findViewById(R.id.hashtag);
         person=(ImageView)rootView.findViewById(R.id.person);
@@ -157,7 +163,6 @@ public class ImageDetailFragment extends Fragment {
 
             }
         });
-
 
         ParseQuery<ParseObject> queryFoto = new ParseQuery<ParseObject>("UserPhoto");
         queryFoto.whereEqualTo("username", immagine.getUser());
@@ -193,71 +198,80 @@ public class ImageDetailFragment extends Fragment {
         query.getFirstInBackground(new GetCallback<ParseObject>() {
             @Override
             public void done(final ParseObject object, ParseException e) {
-                    //setto username e listener
-                    t.setText(immagine.getUser());
-                    t.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent i = ProfileUtils.goToProfile(getActivity().getApplicationContext(),immagine.getUser());
-                            startActivity(i);
-                        }
-                    });
-
-                    //listener on the profile pic
-                    profilePic.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            Intent i = ProfileUtils.goToProfile(getActivity().getApplicationContext(),immagine.getUser());
-                            startActivity(i);
-                            getActivity().finish();
-                        }
-                    });
-
-                    //setto gli hashtag
-                    ArrayList tag = (ArrayList) object.get("hashtag");
-                    String s = " ";
-                    if (tag != null) {
-                        for (int i = 0; i < tag.size(); i++) {
-                            s += tag.get(i).toString() + " ";
-                        }
+                //setto username e listener
+                t.setText(immagine.getUser());
+                t.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = ProfileUtils.goToProfile(getActivity().getApplicationContext(),immagine.getUser());
+                        startActivity(i);
                     }
+                });
 
-                    hashtag.setText((CharSequence) s);
-
-                    //per ogni vestito cerco le informazioni
-                    ArrayList arrayList = (ArrayList) object.get("vestiti");
-                    if (arrayList == null) arrayList = new ArrayList<Cloth>();
-                    vestiti = new ArrayList<Cloth>(arrayList.size());
-                    for (int i = 0; i < arrayList.size(); i++) {
-                        ParseQuery<ParseObject> query1 = new ParseQuery<ParseObject>("Vestito");
-                        query1.whereEqualTo("objectId", arrayList.get(i));
-                        query1.getFirstInBackground(new GetCallback<ParseObject>() {
-                            @Override
-                            public void done(ParseObject info, ParseException e) {
-                                if (e == null) {
-                                    Cloth c = new Cloth(info.getString("tipo"),
-                                            info.getString("luogoAcquisto"),
-                                            info.getString("prezzo"),
-                                            info.getString("shop"),
-                                            info.getString("shopUsername"),
-                                            info.getString("brand"));
-                                    vestiti.add(c);
-                                    MyCardListAdapter adapter = new MyCardListAdapter(context, vestiti);
-                                    listView.setAdapter(adapter);
-                                    setListViewHeightBasedOnItems(listView);
-                                }
-
-                            }
-                        });
+                //listener on the profile pic
+                profilePic.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = ProfileUtils.goToProfile(getActivity().getApplicationContext(),immagine.getUser());
+                        startActivity(i);
+                        getActivity().finish();
                     }
+                });
 
-                    object.getParseFile("photo").getFileInBackground(new GetFileCallback() {
+                //setto gli hashtag
+                ArrayList tag = (ArrayList) object.get("hashtag");
+                String s = " ";
+                if (tag != null) {
+                    for (int i = 0; i < tag.size(); i++) {
+                        s += tag.get(i).toString() + " ";
+                    }
+                }
+
+                hashtag.setText((CharSequence) s);
+
+                //per ogni vestito cerco le informazioni
+                ArrayList arrayList = (ArrayList) object.get("vestiti");
+                if (arrayList == null) arrayList = new ArrayList<Cloth>();
+                vestiti = new ArrayList<Cloth>(arrayList.size());
+                for (int i = 0; i < arrayList.size(); i++) {
+                    ParseQuery<ParseObject> query1 = new ParseQuery<ParseObject>("Vestito");
+                    query1.whereEqualTo("objectId", arrayList.get(i));
+                    query1.getFirstInBackground(new GetCallback<ParseObject>() {
                         @Override
-                        public void done(final File file, ParseException e) {
+                        public void done(ParseObject info, ParseException e) {
                             if (e == null) {
-                                Glide.with(context)
-                                        .load(file)
-                                        .into(v);
+                                Cloth c = new Cloth(info.getString("tipo"),
+                                    info.getString("luogoAcquisto"),
+                                    info.getString("prezzo"),
+                                    info.getString("shop"),
+                                    info.getString("shopUsername"),
+                                    info.getString("brand"));
+                                vestiti.add(c);
+                                MyCardListAdapter adapter = new MyCardListAdapter(context, vestiti);
+                                listView.setAdapter(adapter);
+                                setListViewHeightBasedOnItems(listView);
+                            }
+                        }
+                    });
+                }
+
+                //Gesture Detector for detecting double tap
+                //code is at the end of page
+                final GestureDetector gd = doubleTapGesture(object);
+
+                object.getParseFile("photo").getFileInBackground(new GetFileCallback() {
+                    @Override
+                    public void done(final File file, ParseException e) {
+                        if (e == null) {
+                            Glide.with(context)
+                                    .load(file)
+                                    .into(imageView);
+                            imageView.setOnTouchListener(new View.OnTouchListener() {
+                                @Override
+                                public boolean onTouch(View v, MotionEvent event) {
+                                    return gd.onTouchEvent(event);
+                                }
+                            });
                             /*TODO: problemi di permesso di lettura
                             //setto il listener sull'icona share
                             share.setOnClickListener(new View.OnClickListener() {
@@ -274,67 +288,60 @@ public class ImageDetailFragment extends Fragment {
                                 }
                             });
                             */
-                            }
                         }
-                    }, new ProgressCallback() {
-                        @Override
-                        public void done(Integer percentDone) {
-                            //passo percentuale
-                            if (percentDone==100)   {
-                                percentuale.setVisibility(View.INVISIBLE);
-                            }
-                            percentuale.setText(percentDone+"%");
-                        }
-                    });
-
-                    //mostro il numero di like
-                    int numLikes = object.getInt("nLike");
-                    //  se ho zero likes scrivo like sennò likes
-                    String singPlur = numLikes == 0 || numLikes == 1? "like" : "likes";
-
-                    like.setText(Integer.toString(numLikes) + " " + singPlur);
-
-                    //controllo se ho messo like sull'attuale foto
-                    final String username = ParseUser.getCurrentUser().getUsername();
-                    if (immagine.getLike().contains(username)) {
-                        cuore.setImageResource(R.mipmap.ic_favorite_white_48dp);
-                    } else {
-                        cuore.setImageResource(R.mipmap.ic_favorite_border_white_48dp);
                     }
-                    //metto i listener sul cuore
-                    cuore.setOnClickListener(new View.OnClickListener() {
-                        @Override
-                        public void onClick(View v) {
-                            if (immagine.getLike().contains(username)) {
-                                //possibile problema di concorrenza sull'oggetto in caso più persone stiano mettendo like contemporaneamente
-                                //rimuovo il like e cambio la lista
-                                LikeRes.deleteLike(object,immagine,username);
+                }, new ProgressCallback() {
+                    @Override
+                    public void done(Integer percentDone) {
+                        //passo percentuale
+                        if (percentDone==100)   {
+                            percentuale.setVisibility(View.INVISIBLE);
+                        }
+                        percentuale.setText(percentDone+"%");
+                    }
+                });
 
-                                cuore.setImageResource(R.mipmap.ic_favorite_border_white_48dp);
+                //mostro il numero di like
+                int numLikes = object.getInt("nLike");
+                //  se ho zero likes scrivo like sennò likes
+                String singPlur = numLikes == 0 || numLikes == 1? "like" : "likes";
 
-                                int numLikes = object.getInt("nLike");
-                                //  se ho zero likes scrivo like sennò likes
-                                String singPlur = numLikes == 0 || numLikes == 1 ? "like" : "likes";
-                                like.setText(Integer.toString(numLikes) + " " + singPlur);
+                like.setText(Integer.toString(numLikes) + " " + singPlur);
 
+                //controllo se ho messo like sull'attuale foto
+                final String username = ParseUser.getCurrentUser().getUsername();
+                if (immagine.getLike().contains(username)) {
+                    cuore.setImageResource(R.mipmap.ic_favorite_white_48dp);
+                } else {
+                    cuore.setImageResource(R.mipmap.ic_favorite_border_white_48dp);
+                }
+                //metto i listener sul cuore
+                cuore.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (immagine.getLike().contains(username)) {
+                            //possibile problema di concorrenza sull'oggetto in caso più persone stiano mettendo like contemporaneamente
+                            //rimuovo il like e cambio la lista
+                            LikeRes.deleteLike(object,immagine,username);
 
-                            } else {
-                                //aggiungo like e aggiorno anche in parse
-                                LikeRes.addLike(object,immagine,username);
-                                cuore.setImageResource(R.mipmap.ic_favorite_white_48dp);
-                            }
-                            //aggiorno il numero di like
-                            //System.out.println("debug: cuore premuto su "+immagine.getObjectId()+" con "+immagine.getNumLike());
-
-                            //like.setText(Integer.toString(object.getInt("nLike")));
+                            cuore.setImageResource(R.mipmap.ic_favorite_border_white_48dp);
 
                             int numLikes = object.getInt("nLike");
                             //  se ho zero likes scrivo like sennò likes
                             String singPlur = numLikes == 0 || numLikes == 1 ? "like" : "likes";
                             like.setText(Integer.toString(numLikes) + " " + singPlur);
-
+                        } else {
+                            //aggiungo like e aggiorno anche in parse
+                            LikeRes.addLike(object,immagine,username);
+                            cuore.setImageResource(R.mipmap.ic_favorite_white_48dp);
                         }
-                    });
+                        //aggiorno il numero di like
+                        int numLikes = object.getInt("nLike");
+                        //  se ho zero likes scrivo like sennò likes
+                        String singPlur = numLikes == 0 || numLikes == 1 ? "like" : "likes";
+                        like.setText(Integer.toString(numLikes) + " " + singPlur);
+                    }
+                });
             }
         });
     }
@@ -370,6 +377,65 @@ public class ImageDetailFragment extends Fragment {
         } else {
             return false;
         }
+    }
 
+    //funzione che ritorna il gestureDetector per il doubletap
+    public GestureDetector doubleTapGesture(final ParseObject object)   {
+        return new GestureDetector(context, new GestureDetector.SimpleOnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                return true;
+            }
+            @Override
+            public boolean onDoubleTap(MotionEvent e) {
+                final String username = ParseUser.getCurrentUser().getUsername();
+                if (immagine.getLike().contains(username)) {
+                    //possibile problema di concorrenza sull'oggetto in caso più persone stiano mettendo like contemporaneamente
+                    //rimuovo il like e cambio la lista
+                    LikeRes.deleteLike(object,immagine,username);
+
+                    cuore.setImageResource(R.mipmap.ic_favorite_border_white_48dp);
+
+                    int numLikes = object.getInt("nLike");
+                    //  se ho zero likes scrivo like sennò likes
+                    String singPlur = numLikes == 0 || numLikes == 1 ? "like" : "likes";
+                    like.setText(Integer.toString(numLikes) + " " + singPlur);
+                } else {
+                    //aggiungo like e aggiorno anche in parse
+                    LikeRes.addLike(object,immagine,username);
+                    cuore.setImageResource(R.mipmap.ic_favorite_white_48dp);
+                }
+                //aggiorno il numero di like
+                int numLikes = object.getInt("nLike");
+                //  se ho zero likes scrivo like sennò likes
+                String singPlur = numLikes == 0 || numLikes == 1 ? "like" : "likes";
+                like.setText(Integer.toString(numLikes) + " " + singPlur);
+
+                //inizializzo animazione del cuore che entra ed esce
+                Animation pulse_fade = AnimationUtils.loadAnimation(context, R.anim.pulse_fade_in);
+                pulse_fade.setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+                        heartAnim.setVisibility(View.VISIBLE);
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        heartAnim.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+                    }
+                });
+                heartAnim.startAnimation(pulse_fade);
+                //likeImg.setImageDrawable(context.getResources().getDrawable(R.drawable.like_active));
+                return true;
+            }
+            @Override
+            public boolean onDoubleTapEvent(MotionEvent e) {
+                return true;
+            }
+        });
     }
 }
