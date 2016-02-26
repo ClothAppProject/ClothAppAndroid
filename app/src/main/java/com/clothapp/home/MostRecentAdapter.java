97549da49dc46b019f9f6794ac17c92be37902e0
c-simone <simoneconia@gmail.com>
2016-02-26 03:03:45 +0100

    Load most recent photos.

--- app/src/main/java/com/clothapp/home/MostRecentAdapter.java
+++ app/src/main/java/com/clothapp/home/MostRecentAdapter.java
@@ -1,61 +1,69 @@
 package com.clothapp.home;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.clothapp.R;
+import com.clothapp.resources.Image;
 
+import java.io.File;
 import java.util.List;
 
 class MostRecentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
 
-    private List<String> mItemList;
+    public List<Image> itemList;
 
-    public MostRecentAdapter(List<String> itemList) {
-        mItemList = itemList;
+    public MostRecentAdapter(List<Image> itemList) {
+        this.itemList = itemList;
     }
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         Context context = parent.getContext();
         View view = LayoutInflater.from(context).inflate(R.layout.fragment_home_most_recent_item, parent, false);
-        return RecyclerItemViewHolder.newInstance(view);
+        return new MostRecentItemViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int position) {
-        RecyclerItemViewHolder holder = (RecyclerItemViewHolder) viewHolder;
-        String itemText = mItemList.get(position);
-        holder.setItemText(itemText);
+        MostRecentItemViewHolder holder = (MostRecentItemViewHolder) viewHolder;
+        Image image = itemList.get(position);
+        holder.setItemText(image.getUser());
+        holder.setItemImage(image.getFile());
     }
 
     @Override
     public int getItemCount() {
-        return mItemList == null ? 0 : mItemList.size();
+        return itemList == null ? 0 : itemList.size();
     }
 
-    static class RecyclerItemViewHolder extends RecyclerView.ViewHolder {
+    static class MostRecentItemViewHolder extends RecyclerView.ViewHolder {
 
-        private final TextView mItemTextView;
+        private final TextView txtTitle;
+        private final ImageView imgPhoto;
 
-        public RecyclerItemViewHolder(final View parent, TextView itemTextView) {
+        public MostRecentItemViewHolder(final View parent) {
             super(parent);
-            mItemTextView = itemTextView;
-        }
-
-        public static RecyclerItemViewHolder newInstance(View parent) {
-            TextView itemTextView = (TextView) parent.findViewById(R.id.fragment_home_most_recent_item_title);
-            return new RecyclerItemViewHolder(parent, itemTextView);
+            txtTitle = (TextView) parent.findViewById(R.id.fragment_home_most_recent_item_title);
+            imgPhoto = (ImageView) parent.findViewById(R.id.fragment_home_most_recent_item_image);
         }
 
         public void setItemText(CharSequence text) {
-            if (mItemTextView != null) {
-                mItemTextView.setText(text);
+            if (txtTitle != null) {
+                txtTitle.setText(text);
             }
+        }
+
+        public void setItemImage(File file) {
+            Bitmap imageBitmap = BitmapFactory.decodeFile(file.getAbsolutePath());
+            imgPhoto.setImageBitmap(imageBitmap);
         }
 
     }
