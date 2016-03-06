df71e42125ff6f4b360c3c4b88c2d2febbb3aa04
jacopo <jack1395@hotmail.it>
2016-03-06 18:54:59 +0100

    aggiunti suggerimenti del tipo vestito

--- app/src/main/java/com/clothapp/upload/InfoListAdapter.java
+++ app/src/main/java/com/clothapp/upload/InfoListAdapter.java
@@ -1,11 +1,13 @@
 package com.clothapp.upload;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.support.v7.widget.CardView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.text.method.KeyListener;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,10 +17,14 @@ import android.widget.AutoCompleteTextView;
 import android.widget.BaseAdapter;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.clothapp.R;
 import com.clothapp.resources.Cloth;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,9 +39,8 @@ public class InfoListAdapter extends BaseAdapter {
 
     private int size=0;
     private ArrayList<Cloth> listCloth=new ArrayList<>();
-
-
-
+    private Resources resources;
+    private String output=null;
 
 
     public InfoListAdapter(Context context) {
@@ -81,9 +86,39 @@ public class InfoListAdapter extends BaseAdapter {
             row = inflater.inflate(R.layout.fragment_upload_infocloth, parent, false);
         }
 
+        resources = context.getResources();
+
+        try
+        {
+            //Load the file from the raw folder - don't forget to OMIT the extension
+            output = LoadFile("categorie", true);
+            //output to LogCat
+            Log.i("test", output);
+        }
+        catch (IOException e)
+        {
+            //display an error toast message
+            Toast toast = Toast.makeText(context, "File: not found!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+/*
+        try
+        {
+            //Load the file from assets folder - don't forget to INCLUDE the extension
+            output = LoadFile("categorie.txt", false);
+            //output to LogCat
+            Log.i("test", output);
+        }
+        catch (IOException e)
+        {
+            //display an error toast message
+            Toast toast = Toast.makeText(context, "File: not found!", Toast.LENGTH_LONG);
+            toast.show();
+        }
+*/
         //adattatore per i suggerimenti
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(row.getContext(),
-                android.R.layout.simple_dropdown_item_1line, COUNTRIES);
+                android.R.layout.simple_dropdown_item_1line, output.split("\n"));
         tipo = (AutoCompleteTextView) row.findViewById(R.id.cloth);
         tipo.setAdapter(adapter);
         //appena si preme una lettera appaiono i suggerimenti. Il minimo è 1
@@ -217,10 +252,45 @@ public class InfoListAdapter extends BaseAdapter {
 
     }
 
-    //prova provvisoria dei suggerimenti
-    private static final String[] COUNTRIES = new String[] {
-            "Belgium", "France", "Italy", "Germany", "Spain"
-    };
+
+
+
+
+
+    //load file from apps res/raw folder or Assets folder
+    public String LoadFile(String fileName, boolean loadFromRawFolder) throws IOException
+    {
+        //Create a InputStream to read the file into
+        InputStream iS;
+
+        if (loadFromRawFolder)
+        {
+            //get the resource id from the file name
+            int rID = resources.getIdentifier("com.clothapp:raw/"+fileName, null, null);
+            //get the file as a stream
+            iS = resources.openRawResource(rID);
+        }
+        else
+        {
+            //get the file as a stream
+            iS = resources.getAssets().open(fileName);
+        }
+
+        //create a buffer that has the same size as the InputStream
+        byte[] buffer = new byte[iS.available()];
+        //read the text file as a stream, into the buffer
+        iS.read(buffer);
+        //create a output stream to write the buffer into
+        ByteArrayOutputStream oS = new ByteArrayOutputStream();
+        //write this buffer to the output stream
+        oS.write(buffer);
+        //Close the Input and Output streams
+        oS.close();
+        iS.close();
+
+        //return the output stream as a String
+        return oS.toString();
+    }
 
 
 }
