b9d83e37dd3409cb8714c97e87084226b51f4a51
ceribbo <giacomoceribelli@gmail.com>
2016-04-21 19:07:12 +0200

    BUG FIX: Se l'app della galleria predefinita non è salvata (ma esce "completa l'azione con") la preview compare comunque
    p.s. aggiornato gradle

--- app/src/main/java/com/clothapp/upload/UploadPhotoActivity.java
+++ app/src/main/java/com/clothapp/upload/UploadPhotoActivity.java
@@ -216,16 +216,6 @@ public class UploadPhotoActivity extends AppCompatActivity implements OnConnecti
             }
         }
 
-        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        // setSupportActionBar(toolbar);
-        // Create the adapter that will return a fragment for each of the three
-        // primary sections of the activity.
-        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
-
-        // Set up the ViewPager with the sections adapter.
-        mViewPager = (ViewPager) findViewById(R.id.container);
-        mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setOffscreenPageLimit(3);
 
     }
 
@@ -272,7 +262,17 @@ public class UploadPhotoActivity extends AppCompatActivity implements OnConnecti
             finish();
         }
 
-        //imageView.setImageBitmap(BitmapUtil.scala(imageBitmap));
+
+        // Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        // setSupportActionBar(toolbar);
+        // Create the adapter that will return a fragment for each of the three
+        // primary sections of the activity.
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
+
+        // Set up the ViewPager with the sections adapter.
+        mViewPager = (ViewPager) findViewById(R.id.container);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        mViewPager.setOffscreenPageLimit(3);
     }
 
 
